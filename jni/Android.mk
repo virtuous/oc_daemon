18bc1362d65599ac9d98793fa8d68b2abc30fb02
Rafi Khardalian <rmk@roobix.net>
2011-11-30 23:38:13 -0800

    Properly support ndk builds.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := virtuous_oc
 LOCAL_SRC_FILES := virtuous_oc.c
-LOCAL_SHARED_LIBRARIES := libcutils libc
+LOCAL_LDLIBS := -llog
+
 include $(BUILD_EXECUTABLE)
 
